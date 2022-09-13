0768dc844a0b0c5ad3d44d60a0753d127f02d6d4
Luigi594 <luishnlacho@gmail.com>
2022-09-13 16:57:51 -0600

    Including tailwind provider, I missed out  before

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
+import * as React from 'react';
 import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import CustomerScreen from './screens/CustomerScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
 
     // @ts-ignore - TailwindProvider is missing a type definition
     // npm run dev:tailwind
-    
+
     <TailwindProvider utilities={utilities}>
-      <CustomerScreen />
+      <NavigationContainer>
+
+        <CustomerScreen />
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
